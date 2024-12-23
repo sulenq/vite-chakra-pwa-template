6df04fd51b65044b29f64cc9e6333298af94c8ca
sulenq <whyimustwriteit@gmail.com>
2024-12-23 14:26:31 +0700

    styling

--- src/constant/sizes.ts
+++ src/constant/sizes.ts
@@ -9,7 +9,7 @@ const responsiveSpacing2 = [10, null, 16];
 const responsiveSpacing2Reverse = [-10, null, -16];
 const fcMaxHeight = `calc(100dvh - (74px * 2))`;
 const drawerbodyMaxH = `calc(100dvh - 176px)`;
-const buttonSize = ["lg", null, "md"];
+const buttonSize = ["xl", null, "md"];
 
 export {
   buttonSize,
