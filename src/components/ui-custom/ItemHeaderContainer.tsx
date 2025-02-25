2c82160f222d5263e19239fb8ed0bcf30f2f01b6
sulenq <fatwalinovera@gmail.com>
2025-02-25 17:25:30 +0700

    styling

--- src/components/ui-custom/ItemHeaderContainer.tsx
+++ src/components/ui-custom/ItemHeaderContainer.tsx
@@ -3,12 +3,13 @@ import CContainer from "../ui-custom/CContainer";
 
 const ItemHeaderContainer = ({ children, ...props }: any) => {
   return (
-    <CContainer px={4} pt={2} pb={0}>
+    <CContainer px={3} pt={2} pb={0}>
       <HStack
         borderBottom={"1px solid"}
         borderColor={"border.muted"}
         justify={"space-between"}
         pb={2}
+        pl={1}
         gap={4}
         minH={"50px"}
         wrap={"wrap"}
