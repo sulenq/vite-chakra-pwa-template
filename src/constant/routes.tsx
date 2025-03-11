c547236293fa0acee9d31681f62b12d0ade84cf2
sulenq <fatwalinovera@gmail.com>
2025-03-11 18:50:37 +0700

    refractor

--- src/constant/routes.tsx
+++ src/constant/routes.tsx
@@ -2,7 +2,7 @@ import MerchantHelpCenterPage from "@/pages/MerchantHelpCenterPage";
 import MerchantHomePage from "@/pages/MerchantHomePage";
 import MerchantInvoicePage from "@/pages/MerchantInvoicePage";
 import MerchantServicePage from "@/pages/MerchantServicePage";
-import PreferenceSettingsPage from "@/pages/PreferenceSettingsPage";
+import PreferenceSettingsPage from "@/pages/settings/PreferenceSettingsPage";
 import RootPage from "@/pages/RootPage";
 import SettingsPage from "@/pages/SettingsPage";
 import { Interface__PrivateRoute, Interface__Route } from "./interfaces";
