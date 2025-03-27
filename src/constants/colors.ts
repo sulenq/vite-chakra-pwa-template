4b9ea3a703dca3ee93c70c959f8d6b7d909a61a5
sulenq <fatwalinovera@gmail.com>
2025-03-27 19:54:44 +0700

    refactor

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -2,19 +2,28 @@ export const CHART_COLORS: string[] = [
   "#63B3ED", // Biru muda
   "#3182CE", // Biru
   "#5A67D8", // Ungu
-  "#4FD1C5", // Turquoise
-  // "#9AE6B4", // Hijau cerah
-  "#68D391", // Hijau muda
+  // "#4FD1C5", // Turquoise
+  "#7DDFA3", // Hijau cerah
+  // "#68D391", // Hijau muda
   "#22c55e", // Hijau
   "#F6E05E", // Kuning
-  "#FBD38D", // Oranye lembut
+  // "#FBD38D", // Oranye lembut
   "#F6AD55", // Oranye ringan
   "#F56565", // Merah cerah
-  "#FC8181", // Merah muda
-  "#FEB2B2", // Pink lembut
+  // "#FC8181", // Merah muda
+  // "#FEB2B2", // Pink lembut
   "#F687B3", // Pink terang
   "#D6BCFA", // Ungu pastel
   "#9F7AEA", // Ungu sedang
   "#D53F8C", // Merah muda gelap
   "#E53E3E", // Merah kecokelatan
 ];
+export const ABS_COLORS = {
+  green: "#22c55e",
+  red: "#ef4444",
+  d0: "#7e7e7e08",
+  d1: "#7e7e7e10",
+  d2: "#7e7e7e20",
+  d3: "#7e7e7e36",
+  dt: "#858585c1",
+};
