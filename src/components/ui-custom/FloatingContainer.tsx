33d378065f02a7793e09a4e15b852e658cf6af6f
sulenq <fatwalinovera@gmail.com>
2025-03-28 14:28:14 +0700

    rerfactor: add animationEntrance options

--- src/components/ui-custom/FloatingContainer.tsx
+++ src/components/ui-custom/FloatingContainer.tsx
@@ -1,25 +1,37 @@
 import { Presence, PresenceProps, StackProps } from "@chakra-ui/react";
 import CContainer from "./CContainer";
+import { useThemeConfig } from "@/context/useThemeConfig";
 
 interface Props extends PresenceProps {
   children?: any;
   open?: boolean;
   containerProps?: StackProps;
+  animationEntrance?: "top" | "bottom";
 }
+
 const FloatingContainer = ({
   children,
   open,
   containerProps,
+  animationEntrance = "top",
   ...props
 }: Props) => {
+  // Components
+  const { themeConfig } = useThemeConfig();
+
+  const animationName =
+    animationEntrance === "top"
+      ? { _open: "slide-from-top, fade-in", _closed: "slide-to-top, fade-out" }
+      : {
+          _open: "slide-from-bottom, fade-in",
+          _closed: "slide-to-bottom, fade-out",
+        };
+
   return (
     <Presence
       lazyMount
       present={open}
-      animationName={{
-        _open: "slide-from-top, fade-in",
-        _closed: "slide-to-top, fade-out",
-      }}
+      animationName={animationName}
       animationDuration="moderate"
       {...props}
     >
@@ -27,7 +39,7 @@ const FloatingContainer = ({
         bg={"body"}
         border={"1px solid"}
         borderColor={"border.muted"}
-        borderRadius={8}
+        borderRadius={themeConfig.radii.container}
         p={1}
         transition={"140ms"}
         pointerEvents={open ? "auto" : "none"}
