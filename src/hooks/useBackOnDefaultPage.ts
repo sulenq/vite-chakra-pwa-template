c75aff8aeac1f229bbe0e88b843562eb7e6dbe45
sulenq <fatwalinovera@gmail.com>
2025-03-30 14:01:44 +0700

    refactor: fix close first nav

--- src/hooks/useBackOnDefaultPage.ts
+++ src/hooks/useBackOnDefaultPage.ts
@@ -3,9 +3,15 @@ import { useLocation, useNavigate } from "react-router-dom";
 const useBackOnDefaultPage = () => {
   const location = useLocation();
   const navigate = useNavigate();
+  // const onFirstNavigation = window.history.state === null;
+  // const navEntries = performance.getEntriesByType(
+  //   "navigation"
+  // ) as PerformanceNavigationTiming[];
+  // const unrefreshedYet = navEntries[0]?.type === "navigate";
+  const historyIdx = window.history.state?.idx;
 
   const handleBackOnDefaultPage = () => {
-    if (location.key === "default") {
+    if (historyIdx === 0) {
       const pathWithoutParams = location.pathname;
       navigate(pathWithoutParams, { replace: true });
     } else {
