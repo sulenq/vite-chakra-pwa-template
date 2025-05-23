13851f4dc87240f2a62c5d567810ded60b692c0a
sulenq <fatwalinovera@gmail.com>
2025-05-23 11:24:51 +0700

    styling

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,4 +1,12 @@
+import CContainer from "@/components/ui-custom/CContainer";
 import DatePickerInput from "@/components/ui-custom/DatePickerInput";
+import {
+  AccordionItem,
+  AccordionItemContent,
+  AccordionItemTrigger,
+  AccordionRoot,
+} from "@/components/ui/accordion";
+import { Span } from "@chakra-ui/react";
 import { useFormik } from "formik";
 
 const TestPage = () => {
@@ -10,7 +18,7 @@ const TestPage = () => {
     },
   });
   return (
-    <>
+    <CContainer px={4}>
       <DatePickerInput
         onConfirm={(input) => {
           formik.setFieldValue("date", input);
@@ -18,7 +26,16 @@ const TestPage = () => {
         inputValue={formik.values.date}
         name="date"
       />
-    </>
+      <AccordionRoot>
+        <AccordionItem value="1">
+          <AccordionItemTrigger>
+            <Span>Item 1</Span>
+          </AccordionItemTrigger>
+
+          <AccordionItemContent>Item 1 content here...</AccordionItemContent>
+        </AccordionItem>
+      </AccordionRoot>
+    </CContainer>
   );
 };
 
